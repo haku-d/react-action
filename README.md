adfd014d6946e3de15a4230bed83b235c7665919
Thanh Dang <thanh@clgt.vn>
2021-04-05 00:12:04 -0400

    Update lib usage

--- README.md
+++ README.md
@@ -2,6 +2,55 @@
 
 This project was inspired by https://redux.js.org/recipes/reducing-boilerplate
 
-### [WIP] Documentation
+### Usage
+
+```javascript
+import { createReducer, createAction, createApiAction } from 'react-action';
+
+// Regular action
+export const toggleSidebarAction = createAction('toggleSidebarAction')
+// Api action
+export const getUsersAction = createApiAction(
+    'getUsersAction',
+    () => ({
+        callAPI: () => axios.get('/users')
+    })
+)
+// Api action with payload
+export const getUserAction = createApiAction(
+    'getUserAction',
+    (id) => ({
+        callAPI: () => axios.get('/users', {id})
+    })
+)
+// More example
+export const updateUserAction = createApiAction(
+    'updateUserAction',
+    ({id, ...data}) => ({
+        callAPI () => axios.put(`/users/${id}`, data)
+    })
+)
+
+const initialState = {
+    toggledSidebar: false,
+    users: []
+}
+
+export createReducer(initialState, {
+    [toggleSidebarAction]: (state) => ({
+        ...state,
+        toggledSidebar: !state.toggledSidebar
+    }),
+    [getUsersAction.success]: (state, {response}) => ({
+        ...state,
+        users: response.data
+    }),
+    [getUsersAction.failure]: (state, {error}) => ({
+        ...state,
+        error: error.message
+    })
+})
+
+```
 
 ### [WIP] Test
--- src/index.js
+++ src/index.js
@@ -1,96 +1,96 @@
 export const createReducer = (initialState, handlers) => {
-    return (state = initialState, action) => {
-      const { type, ...rest } = action;
-      if (handlers[type] !== undefined) {
-        return handlers[type](state, rest);
-      }
-      return state;
-    };
-  };
-  
-  export const createAction = (actionName) => {
-    const action = (data) => ({
-      type: actionName,
-      payload: data
-    });
-  
-    return Object.assign(action, {
-      toString: () => actionName
-    });
-  };
-  
-  export const createApiAction = (actionName, actionCreator) => {
-    const requestType = `${actionName}.request`;
-    const successType = `${actionName}.success`;
-    const failureType = `${actionName}.failure`;
-    const apiAction = (payload) => ({
-      ...actionCreator(payload),
+  return (state = initialState, action) => {
+    const { type, ...rest } = action
+    if (handlers[type] !== undefined) {
+      return handlers[type](state, rest)
+    }
+    return state
+  }
+}
+
+export const createAction = (actionName) => {
+  const action = (data) => ({
+    type: actionName,
+    payload: data,
+  })
+
+  return Object.assign(action, {
+    toString: () => actionName,
+  })
+}
+
+export const createApiAction = (actionName, actionCreator) => {
+  const requestType = `${actionName}.request`
+  const successType = `${actionName}.success`
+  const failureType = `${actionName}.failure`
+  const apiAction = (payload) => ({
+    ...actionCreator(payload),
+    payload,
+    types: [requestType, successType, failureType],
+  })
+
+  return Object.assign(apiAction, {
+    request: requestType,
+    success: successType,
+    failure: failureType,
+  })
+}
+
+export const loggerMiddleware = () => {
+  return (next) => (action) => {
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      console.debug(action)
+    }
+    next(action)
+  }
+}
+
+export const callAPIMiddleware = ({ dispatch, getState }) => {
+  return (next) => async (action) => {
+    const { types, callAPI, shouldCallAPI = () => true, payload = {} } = action
+
+    if (!types) {
+      // Normal action: pass it on
+      return next(action)
+    }
+
+    if (
+      !Array.isArray(types) ||
+      types.length !== 3 ||
+      !types.every((type) => typeof type === 'string')
+    ) {
+      throw new Error('Expected an array of three string types.')
+    }
+
+    if (typeof callAPI !== 'function') {
+      throw new Error('Expected callAPI to be a function.')
+    }
+
+    if (!shouldCallAPI(getState())) {
+      return
+    }
+
+    const [requestType, successType, failureType] = types
+
+    dispatch({
       payload,
-      types: [requestType, successType, failureType]
-    });
-  
-    return Object.assign(apiAction, {
-      request: requestType,
-      success: successType,
-      failure: failureType
-    });
-  };
-  
-  export const loggerMiddleware = () => {
-    return (next) => (action) => {
-      if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.debug(action);
-      }
-      next(action);
-    };
-  };
-  
-  export const callAPIMiddleware = ({ dispatch, getState }) => {
-    return (next) => async (action) => {
-      const { types, callAPI, shouldCallAPI = () => true, payload = {} } = action;
-  
-      if (!types) {
-        // Normal action: pass it on
-        return next(action);
-      }
-  
-      if (
-        !Array.isArray(types) ||
-        types.length !== 3 ||
-        !types.every((type) => typeof type === 'string')
-      ) {
-        throw new Error('Expected an array of three string types.');
-      }
-  
-      if (typeof callAPI !== 'function') {
-        throw new Error('Expected callAPI to be a function.');
-      }
-  
-      if (!shouldCallAPI(getState())) {
-        return;
-      }
-  
-      const [requestType, successType, failureType] = types;
-  
+      type: requestType,
+    })
+    try {
+      const response = await callAPI()
       dispatch({
         payload,
-        type: requestType
-      });
-      try {
-        const response = await callAPI();
-        dispatch({
-          payload,
-          response,
-          type: successType
-        });
-      } catch (error) {
-        dispatch({
-          payload,
-          error,
-          type: failureType
-        });
-        throw error;
-      }
-    };
-  };
+        response,
+        type: successType,
+      })
+    } catch (error) {
+      dispatch({
+        payload,
+        error,
+        type: failureType,
+      })
+      throw error
+    }
+  }
+}
